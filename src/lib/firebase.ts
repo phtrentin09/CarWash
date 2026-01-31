9acf4bae4ac1d0eb51cea00612e6c3b016904876
Pedro Medrado <phtrentinmedrado@gmail.com>
2026-01-31 00:12:01 -0300

    Adiciona firebase.ts com configuração do Firebase
    Adiciona firebase.ts com configuração do Firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,21 @@
+// src/lib/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDlH9RMviJETySOwboq_i2YGiGL6dgZrYQ",
+  authDomain: "studio-1013901750-3bbd2.firebaseapp.com",
+  projectId: "studio-1013901750-3bbd2",
+  storageBucket: "studio-1013901750-3bbd2.firebasestorage.app",
+  messagingSenderId: "465432268517",
+  appId: "1:465432268517:web:e48f6751c3a41aef608e0c"
+};
+
+// Singleton (evita inicializar duas vezes)
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export default app;
+
